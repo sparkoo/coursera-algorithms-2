7063baa5a93f7d5865c6c740b8390a01ca086267
Michal Vala <michal.vala@gmail.com>
2018-06-18 05:52:35 +0200

    initial commit, prepare

---
+++ setAndCompile.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+JAVA=java
+JAVAC=javac
+
+SEP=":"
+
+OS=`uname -s`
+case "$OS" in
+  Linux )
+    SEP=":"
+    ;;
+  Windows* | CYGWIN* )
+    SEP=";"
+    ;;
+  * )
+    echo "Unrecognized system!"
+    exit 1;
+    ;;
+esac
+
+OUT_DIR=out/
+LIBS_DIR=../libs
+DATA_DIR=lesson_data/${LESSON_DATA_DIR}
+ALGS4JAR=${LIBS_DIR}/algs4.jar
+
+rm -rf ${OUT_DIR}
+mkdir -p ${OUT_DIR}
+
+javac -cp ${ALGS4JAR} -d out $( find . -name "*.java" )
+
+ls -l out
