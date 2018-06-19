05f994bce279df73316e49f7fa8be5d5a60abbb7
Michal Vala <mvala@redhat.com>
2018-06-19 10:51:23 +0200

    wordnet - read hypernyms to WordNet

--- 01-wordnet/run.sh
+++ 01-wordnet/run.sh
@@ -4,13 +4,13 @@ set -x
 set -e
 
 # write here directory inside lesson data zip archive
-LESSON_DATA_DIR=
+LESSON_DATA_DIR=wordnet
 
 source ../setAndCompile.sh
 
 # run whatever here. example:
+${JAVA} -cp "${ALGS4JAR}${SEP}${OUT_DIR}" WordNet ${DATA_DIR}/synsets15.txt ${DATA_DIR}/hypernyms15Path.txt
 # ${JAVA} -cp "${ALGS4JAR}${SEP}${OUT_DIR}" Outcast
 # ${JAVA} -cp "${ALGS4JAR}${SEP}${OUT_DIR}" SAP
-# ${JAVA} -cp "${ALGS4JAR}${SEP}${OUT_DIR}" WordNet
 
 zip wordnet src/*.java
--- 01-wordnet/src/WordNet.java
+++ 01-wordnet/src/WordNet.java
@@ -1,7 +1,47 @@
+import edu.princeton.cs.algs4.In;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.LinkedList;
+
 public class WordNet {
+
+    private final Map<Integer, List<String>> idToNounMap = new HashMap<>();
+    private final Map<String, Integer> nounToIdMap = new HashMap<>();
+    private final Digraph hypernymsGraph;
 
     // constructor takes the name of the two input files
     public WordNet(String synsets, String hypernyms) {
+        readSynsets(synsets);
+        hypernymsGraph = new Digraph(idToNounMap.size());
+
+        readHypernyms(hypernyms);
+
+
+        System.out.println(idToNounMap);
+        System.out.println(nounToIdMap);
+    }
+
+    private void readSynsets(String synsetsFile) {
+        In in = new In(synsetsFile);
+        while (!in.isEmpty()) {
+            String line = in.readLine();
+            String[] lineParts = line.split(",");
+            Integer id = Integer.parseInt(lineParts[0]);
+            String[] nouns = lineParts[1].split(" ");
+            for (String noun: nouns) {
+                if (!idToNounMap.containsKey(id)) {
+                    idToNounMap.put(id, new LinkedList<String>());
+                }
+                idToNounMap.get(id).add(noun);
+
+                nounToIdMap.put(noun, id);
+            }
+        }
+    }
+
+    private void readHypernyms(String hypernymsFile) {
 
     }
 
@@ -28,6 +68,6 @@ public class WordNet {
 
     // do unit testing of this class
     public static void main(String[] args) {
-
+        new WordNet(args[0], args[1]);
     }
 }
