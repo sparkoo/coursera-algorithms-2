e74d80a66f697d7b0e559a9e5024ab33e2f68a68
Michal Vala <mvala@redhat.com>
2018-06-22 09:34:00 +0200

    wordnet - shortest ancestor and lengt in SAP

--- 01-wordnet/run.sh
+++ 01-wordnet/run.sh
@@ -9,8 +9,8 @@ LESSON_DATA_DIR=wordnet
 source ../setAndCompile.sh
 
 # run whatever here. example:
-${JAVA} -cp "${ALGS4JAR}${SEP}${OUT_DIR}" WordNet ${DATA_DIR}/synsets15.txt ${DATA_DIR}/hypernyms15Path.txt
+#${JAVA} -cp "${ALGS4JAR}${SEP}${OUT_DIR}" WordNet ${DATA_DIR}/synsets15.txt ${DATA_DIR}/hypernyms15Path.txt
 # ${JAVA} -cp "${ALGS4JAR}${SEP}${OUT_DIR}" Outcast
-# ${JAVA} -cp "${ALGS4JAR}${SEP}${OUT_DIR}" SAP
+${JAVA} -cp "${ALGS4JAR}${SEP}${OUT_DIR}" SAP lesson_data/digraph25.txt
 
 zip wordnet src/*.java
--- 01-wordnet/src/SAP.java
+++ 01-wordnet/src/SAP.java
@@ -1,25 +1,64 @@
 import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.BreadthFirstDirectedPaths;
+import edu.princeton.cs.algs4.DirectedDFS;
 
 public class SAP {
 
+    private final Digraph graph;
+
+
     // constructor takes a digraph (not necessarily a DAG)
     public SAP(Digraph G) {
-
+        this.graph = G;
     }
 
     // length of shortest ancestral path between v and w; -1 if no such path
     public int length(int v, int w) {
-        return 0;
+        int closestAncestorId = ancestor(v, w);
+
+        if (closestAncestorId > -1) {
+            DirectedDFS dfsV = new DirectedDFS(graph, v);
+            DirectedDFS dfsW = new DirectedDFS(graph, w);
+
+            return dfsV.distTo(closestAncestorId) + dfsW.distTo(closestAncestorId);
+        } else {
+            return -1;
+        }
     }
 
     // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
     public int ancestor(int v, int w) {
-        return 0;
+        int closestAncestorId = -1;
+
+        DirectedDFS dfsV = new DirectedDFS(graph, v);
+        DirectedDFS dfsW = new DirectedDFS(graph, w);
+
+        for (int i = 0; i < graph.V(); i++) {
+            if (dfsW.marked(i) && dfsV.marked(i)) {
+                if (closestAncestorId == -1) {
+                    closestAncestorId = i;
+                } else {
+                    if (dfsV.distTo(i) < dfsV.distTo(closestAncestorId)) {
+                        closestAncestorId = i;
+                    }
+                }
+            }
+        }
+
+        return closestAncestorId;
     }
 
     // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
     public int length(Iterable<Integer> v, Iterable<Integer> w) {
-        return 0;
+        int shortestPath = -1;
+
+        for (int vI : v) {
+            for (int wI : w) {
+                // TODO
+            }
+        }
+
+        return shortestPath;
     }
 
     // a common ancestor that participates in shortest ancestral path; -1 if no such path
